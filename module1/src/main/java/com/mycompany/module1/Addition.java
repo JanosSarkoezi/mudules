9a5ae79132110a8333376889f3817710c38a900f
JanosSarkoezi <j.sarkoezi@googlemail.com>
2015-11-21 21:20:36 +0100

    added Addittion and Substraction classes

---
+++ module1/src/main/java/com/mycompany/module1/Addition.java
@@ -0,0 +1,12 @@
+package com.mycompany.module1;
+
+/**
+ *
+ * @author saj
+ */
+public class Addition {
+
+    public Integer calculate(Integer a, Integer b) {
+        return a + b;
+    }
+}
---
+++ module2/src/main/java/com/mycompany/module2/Substraction.java
@@ -0,0 +1,12 @@
+package com.mycompany.module2;
+
+/**
+ *
+ * @author saj
+ */
+public class Substraction {
+
+    public Integer calculate(Integer a, Integer b) {
+        return a - b;
+    }
+}
